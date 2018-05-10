a05b0cb19baac8faaf5f3acc1d0a65581adc821c
Carmen López <calope03@ucm.es>
2018-05-10 23:22:11 +0200

    pintados en el mapa los enemigos

---
+++ data/esqueleto.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="esqueleto" tilewidth="25" tileheight="46" tilecount="2" columns="2">
+ <image source="../images/esqueleto_caminando.png" width="50" height="46"/>
+ <tile id="0">
+  <properties>
+   <property name="Class" value="Esqueleto"/>
+  </properties>
+ </tile>
+</tileset>
