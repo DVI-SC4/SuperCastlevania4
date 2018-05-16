6f036603d2a424b5dfb88baa9c6121a0212596d5
Sergio Pino <sergio@pino.es>
2018-05-17 00:01:43 +0200

    Feature: añadida destrucción de enemy.
    Cuando atacas con el látigo el enemigo desaparece.

--- core/entities/components/enemy.js
+++ core/entities/components/enemy.js
@@ -1,13 +1,18 @@
 Quintus.CastlevaniaEnemy = function(Q) {
     Q.component('enemy', {
         added: function() {
-            this.entity.on('bump.left', function(collision) {
+            this.entity.on('bump.left, bump.right', function(collision) {
                 if (collision.obj.isA('Simon')) {
                     this.p.vx = 0;
-                    collision.obj.p.vy = -200;
+                    collision.obj.p.vx = -1000;
                     this.p.vx = -50;
                 }
             });
+            this.entity.on('hit.sprite', function (collision) {
+                if (collision.obj.isA('Whip')) {
+                    this.destroy();
+                }
+            })
         }
     });
 };
