7d168c642c28189a809d7d38e56f88aad5ecbd59
Sergio Pino <sergio@pino.es>
2018-05-17 15:55:34 +0200

    Feature: empezando con animación muerte enemigos.

--- core/entities/components/enemy.js
+++ core/entities/components/enemy.js
@@ -1,6 +1,14 @@
 Quintus.CastlevaniaEnemy = function(Q) {
     Q.load({
-        'enemy_destroyed': 'enemy_destroyed.ogg'
+        'enemy_destroyed': 'enemy_destroyed.ogg',
+        'muerte_enemigo.png': 'muerte_enemigo.png',
+        'muerte_enemigo.json': 'muerte_enemigo.json'
+    }, function () {
+        Q.compileSheets('muerte_enemigo.png', 'muerte_enemigo.json');
+    });
+
+    Q.animations('muerte_enemigo', {
+        muerte: { frames: [0,1,2,3,4], rate: 1/15 }
     });
 
     Q.component('enemy', {
@@ -15,6 +23,8 @@ Quintus.CastlevaniaEnemy = function(Q) {
             this.entity.on('hit.sprite', function (collision) {
                 if (collision.obj.isA('Whip')) {
                     Q.audio.play('enemy_destroyed');
+                    this.p.sheet = 'muerte_enemigo';
+                    this.play('muerte');
                     this.destroy();
                 }
             })
