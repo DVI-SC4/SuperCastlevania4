79c7b00e36b0731f63a19efc71f3997f920b0459
Carmen López <calope03@ucm.es>
2018-05-21 16:05:03 +0200

    añadida escena 3 de prueba

---
+++ data/tiled2v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiled2v2" tilewidth="8" tileheight="8" tilecount="28616" columns="511">
+ <image source="../images/stage-2-lvl-1.png" width="4092" height="448"/>
+</tileset>
